4b80139793bf84571c0e0f87b0728dc4c7cb4655
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-10-02 07:15:41 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,163 @@
+--[[
+    TokenTier Localization: ruRU (Русский)
+
+    Translators: ZamestoTV
+
+      - 
+--]]
+
+local L = LibStub("AceLocale-3.0"):NewLocale("ValuableQuestReward", "ruRU")
+if not L then return end
+
+L["Show armor type overlay"] = "Показывать наложение типа брони"
+L["Colors quest rewards that do not match the item specialization."] = "Окрашивает награды заданий, которые не соответствуют специализации предмета."
+L["Overlay Color"] = "Цвет наложения"
+L["Set the color for the overlay."] = "Установить цвет для наложения."
+L["Icon for specialization"] = "Иконка специализации"
+L["Show icons for which specialization the item is most likely fitting."] = "Показывать иконки для специализаций, под которые предмет наиболее подходит."
+L["Ignore melee weapons"] = "Игнорировать оружие ближнего боя"
+L["None"] = "Нет"
+L["One handed weapons"] = "Одноручное оружие"
+L["Two handed weapons"] = "Двуручное оружие"
+L["Ignore this spec"] = "Игнорировать эту специализацию"
+L["Shows a block panel on items for this spec, nevertheless the spec icon will be shown. It's possible, that this option is ignored for items that are useful for other specs too."] = "Показывает блокирующую панель на предметах для этой специализации, тем не менее иконка специализации будет отображена. Возможно, эта опция будет игнорироваться для предметов, полезных для других специализаций."
+
+--Base stats
+L["Agility"] = "Ловкость"
+L["Declares agility as useful stat. This option has no effect, if the item has strength aswell."] = "Объявляет ловкость полезной характеристикой. Эта опция не имеет эффекта, если предмет имеет силу."
+L["Declares agility as useful stat. This option has no effect, if the item has strength or tank stats aswell."] = "Объявляет ловкость полезной характеристикой. Эта опция не имеет эффекта, если предмет имеет силу или танковые характеристики."
+L["Spirit"] = "Дух"
+L["Declares spririt as useful stat."] = "Объявляет дух полезной характеристикой."
+L["Strength"] = "Сила"
+L["Declares strength as useful stat. This option has no effect, if the item has agility aswell."] = "Объявляет силу полезной характеристикой. Эта опция не имеет эффекта, если предмет имеет ловкость."
+L["Declares strength as useful stat. This option has no effect, if the item has agility or tank stats aswell."] = "Объявляет силу полезной характеристикой. Эта опция не имеет эффекта, если предмет имеет ловкость или танковые характеристики."
+
+--Second stats
+L["Haste rating"] = "Рейтинг скорости боя"
+L["Declares haste rating as useful stat."] = "Объявляет уровень скорости полезной характеристикой."
+L["Crit rating"] = "Рейтинг критического удара"
+L["Declares crit rating as useful stat."] = "Объявляет уровень критического удара полезной характеристикой."
+L["Attack Power"] = "Сила атаки"
+L["Declares attack power as useful stat. This option has no effect, if the item has strength aswell."] = "Объявляет силу атаки полезной характеристикой. Эта опция не имеет эффекта, если предмет имеет силу."
+L["Declares attack power as useful stat. This option has no effect, if the item has agility aswell."] = "Объявляет силу атаки полезной характеристикой. Эта опция не имеет эффекта, если предмет имеет ловкость."
+L["Spellpower"] = "Сила заклинаний"
+L["Declares spellpower as useful stat. This option has no effect, if the item has agility aswell."] = "Объявляет силу заклинаний полезной характеристикой. Эта опция не имеет эффекта, если предмет имеет ловкость."
+--Armor only
+L["Armor: Haste rating"] = "Броня: Рейтинг скорости боя"
+L["Declares haste rating as useful stat on armor."] = "Объявляет уровень скорости полезной характеристикой на броне."
+L["Armor: Crit rating"] = "Броня: Рейтинг критического удара"
+L["Declares crit rating as useful stat on armor."] = "Объявляет уровень критического удара полезной характеристикой на броне."
+L["Armor: Attack Power"] = "Броня: Сила атаки"
+L["Declares attack power as useful stat on armor."] = "Объявляет силу атаки полезной характеристикой на броне."
+L["Armor: Spellpower"] = "Броня: Сила заклинаний"
+L["Declares spellpower as useful stat on armor."] = "Объявляет силу заклинаний полезной характеристикой на броне."
+--Weapons only
+L["Weapons: Haste rating"] = "Оружие: Рейтинг скорости боя"
+L["Declares haste rating as useful stat on weapons."] = "Объявляет уровень скорости полезной характеристикой на оружии."
+L["Weapons: Crit rating"] = "Оружие: Рейтинг критического удара"
+L["Declares crit rating as useful stat on weapons."] = "Объявляет уровень критического удара полезной характеристикой на оружии."
+L["Weapons: Attack Power"] = "Оружие: Сила атаки"
+L["Declares attack power as useful stat on weapons."] = "Объявляет силу атаки полезной характеристикой на оружии."
+L["Weapons: Spellpower"] = "Оружие: Сила заклинаний"
+L["Declares spellpower as useful stat on weapons."] = "Объявляет силу заклинаний полезной характеристикой на оружии."
+
+--Tank stats
+L["Dodge rating"] = "Уровень уклонения"
+L["Declares dodge rating as useful stat."] = "Объявляет уровень уклонения полезной характеристикой."
+L["Parry rating"] = "Уровень парирования"
+L["Declares parry rating as useful stat."] = "Объявляет уровень парирования полезной характеристикой."
+L["Block rating"] = "Уровень блокирования"
+L["Declares block rating as useful stat."] = "Объявляет уровень блокирования полезной характеристикой."
+L["Armor: Dodge rating"] = "Броня: Уровень уклонения"
+L["Declares dodge rating as useful stat on armor."] = "Объявляет уровень уклонения полезной характеристикой на броне."
+L["Armor: Parry rating"] = "Броня: Уровень парирования"
+L["Declares parry rating as useful stat on armor."] = "Объявляет уровень парирования полезной характеристикой на броне."
+L["Armor: Block rating"] = "Броня: Уровень блокирования"
+L["Declares block rating as useful stat on armor."] = "Объявляет уровень блокирования полезной характеристикой на броне."
+L["Weapons: Dodge rating"] = "Оружие: Уровень уклонения"
+L["Declares dodge rating as useful stat on weapons."] = "Объявляет уровень уклонения полезной характеристикой на оружии."
+L["Weapons: Parry rating"] = "Оружие: Уровень парирования"
+L["Declares parry rating as useful stat on weapons."] = "Объявляет уровень парирования полезной характеристикой на оружии."
+L["Weapons: Block rating"] = "Оружие: Уровень блокирования"
+L["Declares block rating as useful stat on weapons. This option has no effect, if the item has agility or tank stats aswell."] = "Объявляет уровень блокирования полезной характеристикой на оружии. Эта опция не имеет эффекта, если предмет имеет ловкость или танковые характеристики."
+
+--Classes
+L["Deathknight"] = "Рыцарь смерти"
+--L["Demonhunter"] = "Охотник на демонов"
+L["Druid"] = "Друид"
+L["Hunter"] = "Охотник"
+L["Mage"] = "Маг"
+L["Monk"] = "Монах"
+L["Paladin"] = "Паладин"
+L["Priest"] = "Жрец"
+L["Rogue"] = "Разбойник"
+L["Shaman"] = "Шаман"
+L["Warlock"] = "Чернокнижник"
+L["Warrior"] = "Воин"
+
+--All Classes Options
+L["Heal"] = "Исцеление"
+
+--Deathknight Options
+L["Blood"] = "Кровь"
+L["Useful stats for blood deathknights"] = "Полезные характеристики для рыцарей смерти специализации Кровь"
+L["Frost/Unholy"] = "Лед/Нечестивость"
+L["Useful stats for frost and unholy deathknights"] = "Полезные характеристики для рыцарей смерти специализаций Лед и Нечестивость"
+
+--Druid Options
+L["Useful stats for heal druids"] = "Полезные характеристики для друидов-целителей"
+L["Moonkin"] = "Баланс"
+L["Useful stats for moonkin druids"] = "Полезные характеристики для друидов специализации Баланс"
+L["Bear"] = "Страж"
+L["Useful stats for bear druids"] = "Полезные характеристики для друидов специализации Страж"
+L["Cat"] = "Сила зверя"
+L["Useful stats for cat druids"] = "Полезные характеристики для друидов специализации Сила зверя"
+
+--Hunter Options
+L["Ignore all melee weapons"] = "Игнорировать все оружие ближнего боя"
+L["Ignores all melee weapons."] = "Игнорирует все оружие ближнего боя."
+
+--Mage Options
+L["Useful stats for mages"] = "Полезные характеристики для магов"
+
+--Monk Options
+L["Brewmaster"] = "Хмелевар"
+L["Useful stats for brewmaster monks"] = "Полезные характеристики для монахов специализации Хмелевар"
+L["Mistweaver"] = "Ткач туманов"
+L["Useful stats for mistweaver monks"] = "Полезные характеристики для монахов специализации Ткач туманов"
+L["Windwalker"] = "Танцующий с ветром"
+L["Useful stats for windwalker monks"] = "Полезные характеристики для монахов специализации Танцующий с ветром"
+
+--Paladin Options
+L["Useful stats for heal paladin"] = "Полезные характеристики для паладинов-целителей"
+L["Protection"] = "Защита"
+L["Useful stats for protection paladin"] = "Полезные характеристики для паладинов специализации Защита"
+L["Retribution"] = "Воздаяние"
+L["Useful stats for retribution paladin"] = "Полезные характеристики для паладинов специализации Воздаяние"
+
+--Priest Options
+L["Useful stats for heal priests"] = "Полезные характеристики для жрецов-целителей"
+L["Shadow"] = "Тьма"
+L["Useful stats for shadow priests"] = "Полезные характеристики для жрецов специализации Тьма"
+
+--Rogue Options
+L["Ignore non-dagger melee weapons"] = "Игнорировать оружие ближнего боя, не являющееся кинжалом"
+L["Ignores all one handed melee weapons that are not a daggers."] = "Игнорирует все одноручное оружие ближнего боя, которое не является кинжалом."
+L["Ignore non-thrown distance weapons"] = "Игнорировать оружие дальнего боя, не являющееся метательным"
+L["Ignores all distance weapons that are not a thrown one."] = "Игнорирует все оружие дальнего боя, которое не является метательным."
+
+--Shaman Options
+L["Useful stats for heal shamans"] = "Полезные характеристики для шаманов-целителей"
+L["Enhancement"] = "Совершенствование"
+L["Useful stats for enhancement shamans"] = "Полезные характеристики для шаманов специализации Совершенствование"
+L["Elemental"] = "Стихии"
+L["Useful stats for elemental shamans"] = "Полезные характеристики для шаманов специализации Стихии"
+
+--Warlock Options
+L["Useful stats for warlocks"] = "Полезные характеристики для чернокнижников"
+
+--Warrior Options
+L["Protection"] = "Защита"
+L["Useful stats for protection warriors"] = "Полезные характеристики для воинов специализации Защита"
+L["Arms/Fury"] = "Оружие/Неистовство"
+L["Useful stats for arms and fury warriors"] = "Полезные характеристики для воинов специализаций Оружие и Неистовство"
